42d98a2d30dca94a35cda85295af6e7d749a1ba3
eeshal236 <eeshalkhan646@gmail.com>
2026-04-25 18:16:35 +0500

    Initial commit

---
+++ intro.py
@@ -0,0 +1,13 @@
+name="Eeshal"
+age=12
+language="python"
+height=5.7
+school="Govt. Girls High School"
+country="Pakistan"
+
+print("My name is ",name)
+print("I am ",age," years old")
+print("I am learning ",language)
+print("My height is ",height)
+print("My school name is ",school)
+print("I live in ",country)
---
+++ math.py
@@ -0,0 +1,8 @@
+a=4
+b=8
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+print(23+45)
