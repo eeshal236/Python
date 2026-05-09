1cf80185d4be93bc0e0146f6a24d72fd75659237
eeshal236 <eeshalkhan646@gmail.com>
2026-05-09 18:25:04 +0500

    initial commit

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+
+a = int(input("Enter the value of a" ))
+b= int(input("Enter the number of b "))
+
+print("The addition of the values is ",add(a,b))
+print("The subtraction of the values is ",sub(a,b))
+print("The multiplication of the values is ",mul(a,b))
+print("The division of the values is ",div(a,b))
---
+++ function.py
@@ -0,0 +1,24 @@
+def name():
+    print("My name is Eeshal")
+
+name()
+
+#2
+def my_name(name):
+    print("My name is", name)
+my_name("Eeshal")
+
+#3
+def intro():
+    a = 23
+    print("Hello! My name is Eeshal")
+    print(a)
+
+intro()
+
+#4
+def all(Name):
+    b = 45
+    print("My name is ",Name)
+    print(b)
+all("Eeshal")
