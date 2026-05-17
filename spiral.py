84093e2112ff78eab205d8485268ecab9c744ca4
eeshal236 <eeshalkhan646@gmail.com>
2026-05-17 17:48:33 +0500

    initial commit

--- spiral.py
+++ spiral.py
@@ -7,13 +7,13 @@ t.speed('fastest')
 t.hideturtle()
 
 while True:
-    for x in range(200):
+    for x in range(400):
         t.pencolor(colors[x%len(colors)])
         t.width(x/100 + 1)
         t.forward(x)
         t.left(59)
     t.right(239)
-    for x in range(200,0,-1):
+    for x in range(400,0,-1):
         t.pencolor('black')
         t.width(x/100 + 7)
         t.forward(x)
